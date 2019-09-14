14b1bf0f22b587b78980ac955d5f4d1bc4ce199a
Gaurav Rane <grane@factset.com>
2019-09-14 17:09:24 -0400

    feat(views): configure new views and removes preset views

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="dashboard">
+    Dashboard
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default class Dashboard extends Vue {
+
+}
+</script>
+
+<style>
+
+</style>

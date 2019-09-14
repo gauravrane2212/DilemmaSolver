69b4e8cb0a28e6fd2f224d3ff4a4d62b206141c6
Gaurav Rane <grane@factset.com>
2019-09-14 17:06:46 -0400

    feat(firebase): adds firebase init logic

---
+++ src/db/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyDfekSAbbW9-eOhkMXaYLAdgH48UDHVWm4',
+  authDomain: 'dilemmasolver-64510.firebaseapp.com',
+  databaseURL: 'https://dilemmasolver-64510.firebaseio.com',
+  projectId: 'dilemmasolver-64510',
+  storageBucket: 'dilemmasolver-64510.appspot.com',
+  messagingSenderId: '3885703102',
+  appId: '1:3885703102:web:d61a3f5919d90c1d7ca3ef',
+};

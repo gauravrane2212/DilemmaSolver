7f08c0f88a06e092f1c059a6e453fdb22da3f443
Gaurav Rane <grane@factset.com>
2019-09-14 20:47:49 -0400

    feat(navbar): adds about and past dilemma buttons to nav bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,35 @@
     dark
     app>
     <v-toolbar-title class="headline text-uppercase">
-      <span>Dilemma Solver</span>
+      <router-link to="/" class="toolbar-title">
+        <span>Dilemma Solver</span>
+      </router-link>
     </v-toolbar-title>
+    <v-spacer/>
+    <v-btn
+      color="white"
+      class="ma-2"
+      to="/about">
+      <v-icon color="teal">mdi-playlist-check</v-icon>
+      <span class="btn-text">Past Dilemmas</span>
+    </v-btn>
+    <v-btn
+      color="white"
+      class="ma-2"
+      to="/about">
+      <v-icon color="teal">mdi-account-circle</v-icon>
+      <span class="btn-text">About</span>
+    </v-btn>
   </v-app-bar>
 </template>
+
+<style scoped>
+.toolbar-title {
+  color: inherit;
+  text-decoration: inherit;
+}
+
+.btn-text {
+  color:teal;
+}
+</style>
